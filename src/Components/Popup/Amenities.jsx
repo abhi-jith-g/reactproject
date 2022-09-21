29e0d5c5c6691dccb261ace39a0454ef6badbe73
abhi-jith-g <you@example.com>
2022-09-21 21:36:00 +0530

    modified:   src/Components/Popup/Amenities.jsx 	modified:   src/Components/Popup/Popup.css 	modified:   src/Components/Popup/RoomPopup.jsx 	modified:   src/Components/Room/Room.css

--- src/Components/Popup/Amenities.jsx
+++ src/Components/Popup/Amenities.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from '../Button/Button'
+import './Popup.css'
 import { useState } from 'react'
 
 
@@ -23,14 +23,32 @@ const Amenities= ({check,data,title,text,index,bgclr,color}) => {
         </select>
         <div className='selectdata'>
           {select.map((data,index)=>{
-            return(
-              <Button className='btnaddnew' text={data} Functionality={()=>{
-                setSelect(select.filter((item,itemindex)=>{
-                  return itemindex !==index;
-                }))
-              }}/>
-            )
-          })}
+              return(
+                <div className='roundlabel'>
+                  <div>
+                  <label>{data}</label>
+                  </div>
+                  <div>
+    <svg 
+    style={{cursor:'pointer'}}
+    onClick={()=>{
+        setSelect(
+            select.filter((item,itemindex)=>{
+                return itemindex !== index;
+            })
+        )
+    }}
+    
+    
+    
+    
+    xmlns="http://www.w3.org/2000/svg" width='20px' height='20px' viewBox="0 0 512 512"><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>
+
+    </div>
+
+                </div>
+              )
+            })}
       
 </div>
         
